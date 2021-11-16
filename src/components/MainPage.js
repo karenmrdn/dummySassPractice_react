0c9088c7499a0ddf51d1197158527b988db4d5cd
karenmrdn <karenmrdn47@gmail.com>
2021-11-16 10:11:04 +0200

    Navbar and main content were styled using SCSS files

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/MainPage.scss";
+
+const MainPage = () => {
+  return (
+    <div className="mainPage">
+      <h1>Welcome to the website</h1>
+      <button>Click here</button>
+    </div>
+  );
+};
+
+export default MainPage;
